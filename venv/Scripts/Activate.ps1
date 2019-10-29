7b05d280d2d1aebd04660568075159dc93218147
Failz8b <55516385+Failz8b@users.noreply.github.com>
2019-10-28 21:41:07 -0700

    Venv

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\miche\PycharmProjects\SuperMarioBrosFinal\venv"
+$env:VIRTUAL_ENV="D:\Projects\Team_BEA\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
