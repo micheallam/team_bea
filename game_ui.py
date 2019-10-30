4af3caf7717350db9ee761182e3942be69cc6ae6
TommyH409 <36341132+TommyH409@users.noreply.github.com>
2019-10-30 13:49:39 -0700

    Update game_ui.py

--- game_ui.py
+++ game_ui.py
@@ -1,6 +1,6 @@
 import pygame
 
-
+# blits words onto the screen 
 class game_ui(object):
     def __init__(self):
         self.font = pygame.font.SysFont(None, 36)
@@ -13,18 +13,21 @@ class game_ui(object):
             main.screen.blit(rect, (x, 0))
             x += 168
 
+# Score count            
         text = self.font.render(str(main.get_map().get_player().score), False, (255, 255, 255))
         rect = text.get_rect(center=(60, 35))
         main.screen.blit(text, rect)
 
+# Coin count        
         text = self.font.render(str(main.get_map().get_player().coins), False, (255, 255, 255))
         rect = text.get_rect(center=(230, 35))
         main.screen.blit(text, rect)
-
+        
         text = self.font.render(main.get_map().get_name(), False, (255, 255, 255))
         rect = text.get_rect(center=(395, 35))
         main.screen.blit(text, rect)
-
+        
+# Clock
         text = self.font.render(str(main.get_map().time), False, (255, 255, 255))
         rect = text.get_rect(center=(557, 35))
         main.screen.blit(text, rect)
