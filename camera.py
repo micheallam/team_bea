6173d9a10138c30ca675719988bbc7d2dfa8cc96
TommyH409 <36341132+TommyH409@users.noreply.github.com>
2019-10-30 13:08:21 -0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,13 +1,14 @@
 import pygame
 from settings import *
 
-
+# This follows mario along the map but cannot lock him in place like the original game
 class Camera(object):
     # Controls the camera moving left and right
     def __init__(self, screen_width, screen_height):
         self.rect = pygame.Rect(0, 0, screen_width, screen_height)
         self.camera(self.rect)
 
+    # Sets the camera view
     def camera(self, screen_rect):
         x, y = screen_rect.x, screen_rect.y
         y = screen_rect.y
@@ -22,9 +23,11 @@ class Camera(object):
 
     def apply(self, window):
         return window.rect.x + self.rect.x, window.rect.y
-
+    
+    # Follows mario's position and updates its own coordinates to track him
     def update(self, window):
         self.rect = self.camera(window)
 
+    # When the game is over
     def reset(self):
         self.rect = pygame.Rect(0, 0, self.rect.w, self.rect.height)
