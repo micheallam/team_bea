95ffa058a621392a551fda38f5e850e141d52743
TommyH409 <36341132+TommyH409@users.noreply.github.com>
2019-10-30 14:11:43 -0700

    Update loading_menu.py

--- loading_menu.py
+++ loading_menu.py
@@ -3,14 +3,15 @@ import pygame
 from settings import *
 from text import text
 
-
+# Screen after the player presses enter to play
 class loading_menu(object):
     def __init__(self, main):
         self.now = pygame.time.get_ticks()
         self.loading_type = True
         self.bg = pygame.Surface((screen_width, screen_height))
         self.text = text('World ' + main.object_world.get_name(), 52, (screen_width / 2, screen_height / 2))
-
+    
+    # Plays the game music
     def update(self, main):
         if pygame.time.get_ticks() >= self.now + (5250 if not self.loading_type else 2500):
             if self.loading_type:
@@ -22,6 +23,7 @@ class loading_menu(object):
                 self.set_text_type('WORLD ' + main.object_world.get_name(), True)
                 main.get_map().reset(True)
 
+    # Centers text on the screen
     def set_text_type(self, message, type):
         self.text = text(message, 32, (screen_width / 2, screen_height / 2))
         self.loading_type = type
@@ -29,6 +31,6 @@ class loading_menu(object):
     def render(self, main):
         main.screen.blit(self.bg, (0, 0))
         self.text.render(main)
-
+    
     def update_time(self):
         self.now = pygame.time.get_ticks()
